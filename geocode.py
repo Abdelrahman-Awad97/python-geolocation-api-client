c4413802abd6e79c65028bacf58e8a8ef82de093
Abdelrahman-Mo97 <101920262+Abdelrahman-Mohamed97@users.noreply.github.com>
2026-03-11 04:55:57 +0100

    uploading files

---
+++ geocode.py
@@ -0,0 +1,54 @@
+import urllib.request, urllib.parse, urllib.error
+import json, ssl
+
+parms = {}
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+basic_domain = 'https://py4e-data.dr-chuck.net/opengeo?'
+
+while True:
+    address = input('Type the location here: ').strip()
+    
+    if len(address) < 1:
+        print('wrong entry')
+        quit()
+    
+    if address == 'done':
+        print('Thank you for using our program')
+        quit()
+    
+    parms['q'] = address
+    url = basic_domain + urllib.parse.urlencode(parms)
+    # print(url)
+
+    fetch_data_b = urllib.request.urlopen(url, context=ctx)
+    fetch_data_s = fetch_data_b.read().decode()
+
+    
+    try:
+        fetch_data_d = json.loads(fetch_data_s)
+    except:
+        fetch_data_d = None
+
+    # to see beautiful json
+    # print(json.dumps(fetch_data_d, indent=4))
+
+    if fetch_data_d == None or 'features' not in fetch_data_d:
+        print('downloading error')
+        quit()
+    
+    features_lst = fetch_data_d['features']
+    if len(features_lst) < 1:
+        print('object not found')
+    
+    # data extraction
+    lon = features_lst[0]['properties']['lon']
+    lat = features_lst[0]['properties']['lat']
+    country = features_lst[0]['properties']['country']
+    state = features_lst[0]['properties']['state']
+    print(f'this is lon {lon}')
+    print(f'this is lon {lat}')
+    print(f'location {country}, {state}')
